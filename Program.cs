fdc65cc78846a4c89c444825e428d75c53ad9efd
wilson Félix de Andrade Júnior <wilson.felix.jr@gmail.com>
2025-02-09 12:51:19 -0300

    Teste de c# - Verificar README dentro da pasta raiz do projeto

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Solution
+{
+    public bool DetectCapitalUse(string word)
+    {
+        return word == word.ToUpper() || word == word.ToLower() ||
+               (char.IsUpper(word[0]) && word.Substring(1) == word.Substring(1).ToLower());
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+
+        while (true)
+        {
+            Console.Write("Digite uma palavra (ou 'sair' para encerrar): ");
+            string word = Console.ReadLine();
+
+            if (word.ToLower() == "sair")
+                break;
+
+            bool result = solution.DetectCapitalUse(word);
+            Console.WriteLine($"Palavra: {word}");
+            Console.WriteLine($"Está correta de acordo com as regras? {result}");
+            Console.WriteLine("-----------------------------------");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # DetectCapital
 Teste de c#
+
+
+Pretensão salarial (em valor /hora) - moeda R$ 75,00
+
+Disponibilidade para início: 10/02/2025
+
+Disponibilidade de horas (informar período) para trabalhar na easy de segunda a sexta-feira: 10 horas / dia
+
+Nivelamento em ingles: B1
+
+Meu CNPJ: 45.436.095/0001-29
